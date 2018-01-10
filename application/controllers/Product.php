9ceda915418cafb3af90879668007652518a6c36
liuyongming <liuyongming@shopex.cn>
2018-01-10 18:10:23 +0800

    添加修改商品属性

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -22,4 +22,36 @@ class Product extends AdController
         $this->vars['category_list'] = $cate_list;
         $this->page("product/category_list.html");
     }
+
+    public function save_product_cate()
+    {
+        $cate_id = $this->input->post("cate_id");
+        $cate_name = $this->input->post("name");
+        $product_cate1 = $this->input->post("product_cate1");
+        $product_cate2 = $this->input->post("product_cate2");
+        $product_cate3 = $this->input->post("product_cate3");
+        $product_cate4 = $this->input->post("product_cate4");
+        if (empty($cate_id)) {
+            $this->json_result(LACK_REQUIRED_PARAMETER, "", "缺少类目id");
+        }
+        if (empty($product_cate1)) {
+            $this->json_result(LACK_REQUIRED_PARAMETER, "", "缺少商品属性一");
+        }
+        if (empty($product_cate2)) {
+            $this->json_result(LACK_REQUIRED_PARAMETER, "", "缺少商品属性二");
+        }
+        if (empty($product_cate3)) {
+            $this->json_result(LACK_REQUIRED_PARAMETER, "", "缺少商品属性三");
+        }
+        if (empty($product_cate4)) {
+            $this->json_result(LACK_REQUIRED_PARAMETER, "", "缺少商品属性四");
+        }
+        $this->load->model('ProductModel', "product", true);
+        $save_status = $this->product->save_prodcut_cate($cate_id, $cate_name, $product_cate1, $product_cate2, $product_cate3, $product_cate4);
+        if ($save_status) {
+            $this->json_result(REQUEST_SUCCESS, "商品属性添加成功");
+        } else {
+            $this->json_result(API_ERROR, "", "服务器出错");
+        }
+    }
 }
--- application/models/ProductModel.php
+++ application/models/ProductModel.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * 文件名称:ProductModel.php
  * 摘    要:
@@ -11,5 +12,15 @@ class ProductModel extends CI_Model
     {
         $query = $this->db->query("SELECT t.id, t.name, t.level, c.cate_name as catename, c.product_cate1,  c.product_cate2,  c.product_cate3,  c.product_cate4 FROM `t_type` AS t LEFT JOIN `t_product_category` AS c ON c.cate_id=t.id WHERE t.level=3 ");
         return $query->result_array();
+    }
+
+    public function save_prodcut_cate($cate_id, $cate_name, $product_cate1, $product_cate2, $product_cate3, $product_cate4)
+    {
+        $this->db->query("INSERT INTO `t_product_category` (cate_id, cate_name, product_cate1, product_cate2, product_cate3, product_cate4, status) VALUES ('{$cate_id}', '{$cate_name}', '{$product_cate1}', '{$product_cate2}', '{$product_cate3}', '{$product_cate4}', 1) ON DUPLICATE KEY UPDATE product_cate1 = VALUES (product_cate1), product_cate2 = VALUES (product_cate2), product_cate3 = VALUES (product_cate3), product_cate4 = VALUES (product_cate4)");
+        if ($this->db->affected_rows() >= 1) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
