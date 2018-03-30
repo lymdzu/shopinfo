ba4520e8bff16b224775584beacd858faf15b974
liuyongming <liuyongming@shopex.cn>
2018-03-30 19:07:31 +0800

    修改更新属性

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -31,6 +31,7 @@ class Product extends AdController
     public function save_product_cate()
     {
         $cate_id = $this->input->post("cate_id");
+        $id = $this->input->post("id");
         $cate_name = $this->input->post("name");
         $product_cate1 = $this->input->post("product_cate1");
         $product_cate2 = $this->input->post("product_cate2");
@@ -52,7 +53,12 @@ class Product extends AdController
             $this->json_result(LACK_REQUIRED_PARAMETER, "", "缺少商品属性四");
         }
         $this->load->model('ProductModel', "product", true);
-        $save_status = $this->product->save_prodcut_cate($cate_id, $cate_name, $product_cate1, $product_cate2, $product_cate3, $product_cate4);
+        if (empty($id)) {
+            $save_status = $this->product->save_prodcut_cate($cate_id, $cate_name, $product_cate1, $product_cate2, $product_cate3, $product_cate4);
+        } else {
+            $save_status = $this->product->update_prodcut_cate($id, $cate_id, $cate_name, $product_cate1, $product_cate2, $product_cate3, $product_cate4);
+        }
+
         if ($save_status) {
             $this->json_result(REQUEST_SUCCESS, "商品属性添加成功");
         } else {
--- application/models/ProductModel.php
+++ application/models/ProductModel.php
@@ -2,15 +2,54 @@
 
 class ProductModel extends CI_Model
 {
+    /**
+     *
+     * @return mixed
+     * @author liuyongming@shopex.cn
+     */
     public function get_product_category()
     {
         $query = $this->db->query("SELECT t.id as cate_id, c.id, t.name, t.level, c.cate_name as catename, c.product_cate1,  c.product_cate2,  c.product_cate3,  c.product_cate4 FROM `t_type` AS t LEFT JOIN `t_product_category` AS c ON c.cate_id=t.id WHERE t.level=3 ");
         return $query->result_array();
     }
 
+    /**
+     *
+     * @param $cate_id
+     * @param $cate_name
+     * @param $product_cate1
+     * @param $product_cate2
+     * @param $product_cate3
+     * @param $product_cate4
+     * @return bool
+     * @author liuyongming@shopex.cn
+     */
     public function save_prodcut_cate($cate_id, $cate_name, $product_cate1, $product_cate2, $product_cate3, $product_cate4)
     {
         $this->db->query("INSERT INTO `t_product_category` (cate_id, cate_name, product_cate1, product_cate2, product_cate3, product_cate4, status) VALUES ('{$cate_id}', '{$cate_name}', '{$product_cate1}', '{$product_cate2}', '{$product_cate3}', '{$product_cate4}', 1) ON DUPLICATE KEY UPDATE product_cate1 = VALUES (product_cate1), product_cate2 = VALUES (product_cate2), product_cate3 = VALUES (product_cate3), product_cate4 = VALUES (product_cate4)");
+        if ($this->db->affected_rows() >= 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     *
+     * @param $id
+     * @param $cate_id
+     * @param $cate_name
+     * @param $product_cate1
+     * @param $product_cate2
+     * @param $product_cate3
+     * @param $product_cate4
+     * @return bool
+     * @author liuyongming@shopex.cn
+     */
+    public function update_prodcut_cate($id, $cate_id, $cate_name, $product_cate1, $product_cate2, $product_cate3, $product_cate4)
+    {
+        log_message("info", "update product cate, id:{$id},cate_id:{$cate_id}, cate_name:{$cate_name}, product_cate1:{$product_cate1}, product_cate2:{$product_cate2}, product_cate3:{$product_cate3}, product_cate4:{$product_cate4}");
+        $this->db->query("UPDATE `t_product_category`  SET cate_id = {$cate_id}, cate_name = '{$cate_name}', product_cate1 = '{$product_cate1}', product_cate2 = '{$product_cate2}', product_cate3 = '{$product_cate3}', product_cate4 = '{$product_cate4}' WHERE id={$id}");
         if ($this->db->affected_rows() >= 1) {
             return true;
         } else {
